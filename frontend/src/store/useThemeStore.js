a6021a1136a9006a67b3db59a59cd409ff1a9de0
saiteja218 <saitejareddy218@gmail.com>
2025-03-28 19:58:27 +0530

    changed default theme

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,7 +1,7 @@
 import {create} from 'zustand'
 
 export const useThemeStore = create((set) => ({
-    theme: localStorage.getItem('theme') || 'light',
+    theme: localStorage.getItem('theme') || 'dark', 
     setTheme:(theme)=>{
         localStorage.setItem('theme', theme)
         set({theme})
